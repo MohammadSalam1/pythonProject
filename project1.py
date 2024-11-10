223493802d53fa945c88bfc34970e0833e37eb76
Mohammad Salam <mhd.salam99@gmail.com>
2024-11-10 21:05:29 +0100

    proj1 calculator proj2 bookshelf

--- project1.py
+++ project1.py
@@ -1,22 +1,22 @@
 #project one. kickstart my journey.
 #let's make a small calculator for no
-fstnum = int(input("chose your first number: "))
-scdnum = int(input("chose your second number: "))
-rslt = float()
-gv_answer = input("choose [add, subtract, multiply, divide]: ")
+#try to comment more to help future self and others
+fstnum = int(input("chose your first number: ")) #asks user for integer (digit)
+scdnum = int(input("chose your second number: ")) #asks for a second number
+gv_answer = input("choose [add, subtract, multiply, divide]: ") #asks user for what arithmetic function they want
 
-if gv_answer == "add":
+if gv_answer == "add": #adds inputs
     rslt = fstnum + scdnum
-    print(rslt)
+    print(rslt) #shows results
 
-if gv_answer == "subtract":
+if gv_answer == "subtract": #subtracts inputs
     rslt = fstnum - scdnum
     print(rslt)
 
-if gv_answer == "multiply":
+if gv_answer == "multiply": #multiplys inputs
     rslt = fstnum * scdnum
     print(rslt)
 
-if gv_answer == "divide":
+if gv_answer == "divide": #divides inputs
     rslt = fstnum / scdnum
     print(rslt)
