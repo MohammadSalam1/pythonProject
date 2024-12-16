4fe550ed5093c432c2b1e9a37013b2032977595e
Mohammad Salam <mhd.salam99@gmail.com>
2024-12-16 17:09:46 +0100

    findafile

--- UniFollowedCode/JSONstuff/findafile.py
+++ UniFollowedCode/JSONstuff/findafile.py
@@ -1,8 +1,8 @@
 filename = input("give me a txt file path:")
 word = input("word to find: ")
 
-with open(filename, encoding="utf-8") as f:
-  content = f.read()
+with open('hello.txt', encoding="utf-8") as f:
+  content = f.readlines()
 
 for i, line in enumerate(content):
   if word in line:
