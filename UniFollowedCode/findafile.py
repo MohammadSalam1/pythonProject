40d3580a2b7378de63f229110e54c013d1d39511
Mohammad Salam <mhd.salam99@gmail.com>
2024-12-16 14:59:19 +0100

    not connected to hello, needs path

---
+++ UniFollowedCode/findafile.py
@@ -0,0 +1,9 @@
+filename = input("give me a txt file path:")
+word = input("word to find: ")
+
+with open(filename, encoding="utf-8") as f:
+  content = f.read()
+
+for i, line in enumerate(content):
+  if word in line:
+    print(i, line, end="")
