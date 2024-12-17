9bc4918c4f5deca72352d4d42238092de48529af
Mohammad Salam <mhd.salam99@gmail.com>
2024-12-17 08:16:08 +0100

    create, write and read files using python with open function

--- UniFollowedCode/findtxtinfile/findafile.py
+++ UniFollowedCode/findtxtinfile/findafile.py
@@ -1,9 +1,15 @@
 filename = input("give me a txt file path:")
 word = input("word to find: ")
 
-with open('hello.txt', encoding="utf-8") as f:
+with open('hello.txt') as f:
   content = f.readlines()
 
+new_content = []
 for i, line in enumerate(content):
   if word in line:
-    print(i, line)
+    new_content.append(str(i) + " " + line)
+
+new_filename = filename + word + ".txt"
+
+with open(new_filename, "w", encoding="utf-8") as f:
+  f.writelines(new_content)
