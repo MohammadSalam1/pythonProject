7e637cb14bdfdb741e024cbde8325db053576abb
Mohammad Salam <mhd.salam99@gmail.com>
2025-01-09 14:07:53 +0100

    questions to get ready for test

---
+++ UniFollowedCode/exam_practice/darwin_test2_practice_q4.py
@@ -0,0 +1,16 @@
+def remove_short_strings(string_list):
+  
+
+
+
+
+
+#Test case 1
+words1 = ["apple", "banana", "grape", "kiwi", "pear", "orange"]
+print(remove_short_strings(words1))
+#Output: ["apple", "banana", "orange", "grape"]
+
+#Test case 2 
+words3 = ["python", "java", "c++", "ruby", "swift"] 
+print(remove_short_strings(words3))
+#Output: ["python", "swift"]
