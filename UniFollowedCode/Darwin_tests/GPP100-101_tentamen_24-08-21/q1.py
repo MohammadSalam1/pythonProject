5ad1d639e800c95251fe6a45b8c74c3d1e4d8852
Mohammad Salam <mhd.salam99@gmail.com>
2025-01-13 13:05:04 +0100

    doing all questions till pass

--- UniFollowedCode/Darwin_tests/GPP100-101_tentamen_24-08-21/q1.py
+++ UniFollowedCode/Darwin_tests/GPP100-101_tentamen_24-08-21/q1.py
@@ -1,6 +1,10 @@
 def calculate_popcorn_cost_in_sek (grams):
+  full_cost = grams * 0.20
 
-  pass
+  price_kr = int(full_cost)
+  left_ore = int((full_cost - price_kr) * 100)
+
+  return (price_kr, left_ore)
 
 
 print(calculate_popcorn_cost_in_sek(750))  # Output: (150, 0) 
---
+++ UniFollowedCode/Darwin_tests/GPP100-101_tentamen_24-08-21/q2.py
@@ -0,0 +1,9 @@
+def calculate_rental_cost (days):
+  
+
+
+
+
+print(calculate_rental_cost(2))  # Output: (70) 
+print(calculate_rental_cost(4))  # Output: (140) 
+print(calculate_rental_cost(12)) # Output: (300)
