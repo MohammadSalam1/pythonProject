a37e73dcd9a03842bcccb481321e7a3ee56a66d6
Mohammad Salam <mhd.salam99@gmail.com>
2025-01-13 11:57:16 +0100

    doing all questions till pass

---
+++ UniFollowedCode/Darwin_tests/GPP100-101_tentamen_24-08-21/q1.py
@@ -0,0 +1,7 @@
+def calculate_popcorn_cost_in_sek (grams):
+
+  pass
+
+
+print(calculate_popcorn_cost_in_sek(750))  # Output: (150, 0) 
+print(calculate_popcorn_cost_in_sek(76))  # Output: (15, 20)
