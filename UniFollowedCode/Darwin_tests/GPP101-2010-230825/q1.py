a2cf8f272b6adc75bb0935572d5d306f65b256c1
Mohammad Salam <mhd.salam99@gmail.com>
2025-01-13 14:22:50 +0100

    second exam practice

---
+++ UniFollowedCode/Darwin_tests/GPP101-2010-230825/q1.py
@@ -0,0 +1,8 @@
+def feeding_status(hours_since_last_meal):
+  
+  pass
+
+print(feeding_status(2))   # Output: "Full" 
+print(feeding_status(4))   # Output: "Full" 
+print(feeding_status(4.5)) # Output: "Hungry" 
+print(feeding_status(6))   # Output: "Unhappy"
