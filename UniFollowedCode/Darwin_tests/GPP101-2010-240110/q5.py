84ec9d3b4e3199459457f731aa170c34483ef46b
Mohammad Salam <mhd.salam99@gmail.com>
2025-01-14 07:22:21 +0100

    practice test

---
+++ UniFollowedCode/Darwin_tests/GPP101-2010-240110/q5.py
@@ -0,0 +1,15 @@
+def scalar_multiply_matrix(matrix, scalar):
+  pass
+
+
+
+
+
+# Test case 
+original_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+print(scalar_multiply_matrix(original_matrix, 2))   
+# Output: [[2, 4, 6], [8, 10, 12], [14, 16, 18]] 
+ 
+original_matrix = [[2, 5, 1], [6, 2, 7], [3, 8, 4]] 
+print(scalar_multiply_matrix(original_matrix, 3))   
+# Output: [[[6, 15, 3], [18, 6, 21], [9, 24, 12]]
